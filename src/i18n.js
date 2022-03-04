914d7797ae4009aa1f7cc5f2d0c0c55def6615d9
atul.shukla@mobisy.com <atul.shukla@mobisy.com>
2022-03-04 21:41:39 +0530

    dashboard widget

--- src/i18n.js
+++ src/i18n.js
@@ -44,6 +44,15 @@ const resources = {
         "UserName": "User Name",
         "name":"Bizom",
         "Logout":"Logout"
+      },
+      "homePage":{
+        "Orders":"Orders",
+        "Recieved":"Recieved",
+        "InProcess":"In Process",
+        "Completed":"Completed",
+        "Pending":"Pending",
+        "Invoices":"Invoices"
+       
       }
       
 
@@ -64,6 +73,14 @@ const resources = {
         "UserName": "Nutzername",
         "name":"Bizom",
         "Logout":"Ausloggen"
+      },
+      "homePage":{
+        "Orders":"আদেশ",
+        "Recieved":"প্রাপ্ত",
+        "InProcess":"প্রক্রিয়াধীন",
+        "Completed":"সম্পন্ন",
+        "Pending":"বিচারাধীন",
+        "Invoices":"চালান"
       }
     }
   }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,70 +1,81 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { Box, Link, Avatar, Card } from '@material-ui/core/';
-import PeopleOutlineOutlinedIcon from '@material-ui/icons/PeopleOutlineOutlined';
-import SpeakerNotesIcon from '@material-ui/icons/SpeakerNotes';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Demo from "../components/buttonUI/demo";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    height: 400,
-    alignItems: "center",
-    maxWidth: "100%"
+    margin: theme.spacing(5, 1)
   },
-  card: {
-    paddingTop: '20%',
-    justifyContent: 'center',
-    height: 200,
-    width: 230,
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   },
-  avatar: {
-    margin: '0 30% 20% 30%',
-    backgroundColor: theme.palette.secondary.main,
-    height: 90,
-    width: 90,
+  backy:{
+    backgroundColor:'#fffde7'
   },
+  backp:{
+    backgroundColor:'#e1f5fe'
+  }
 })
 );
 
-export default function HomePage() {
+export default function HomePage(props) {
+  const {t, i18n} = props
   const spacing = 2;
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.root} spacing={1}>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={spacing} >
-
-          <Grid item>
-            <Card className={classes.card}>
-              <Avatar className={classes.avatar}>
-                <PeopleOutlineOutlinedIcon />
-              </Avatar>
-              <Box component="span" m={10}>
-                <Button varient='outlined' color='primary'>
-                  <Link varient='outlined' color='primary' href='/users/'>
-                    Users
-                  </Link>
-                </Button>
-              </Box>
-            </Card>
-          </Grid>
-
-          <Grid item>
-            <Card className={classes.card}>
-              <Avatar className={classes.avatar}>
-                <SpeakerNotesIcon />
-              </Avatar>
-              <Box component="span" m={10}>
-                <Button><Link href='./blogs/'>Blogs</Link></Button>
-              </Box>
-            </Card>
-          </Grid>
-
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} >
+          <Paper className={`${classes.paper} ${classes.backy}`}>
+            <Typography align="left" color="textPrimary">{t('homePage.Orders')}</Typography>            
+              <Demo
+                labelone={t('homePage.Recieved')}
+                valone={1000}
+                labeltwo={t('homePage.InProcess')}
+                valtwo={400}
+                labelthree={t('homePage.Completed')}
+                valthree={600}/>           
+          </Paper>
         </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={`${classes.paper} ${classes.backp}`}>
+            <Typography align="left" color="textPrimary">{t('homePage.Invoices')}</Typography>            
+              <Demo
+                 labelone={t('homePage.Pending')}
+                 valone={100}
+                 labeltwo={t('homePage.InProcess')}
+                 valtwo={40}
+                 labelthree={t('homePage.Completed')}
+                 valthree={60}/>           
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6} >
+        <Paper className={`${classes.paper}`}>
+        1
+        </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3} >
+          <Paper className={`${classes.paper}`}>
+        2
+        </Paper>
+          </Grid>
+          <Grid item xs={6} sm={3} >
+          <Paper className={`${classes.paper}`}>
+       3
+        </Paper>
+          </Grid>
+       
+       
       </Grid>
-    </Grid>
+    </div>
   );
 }
